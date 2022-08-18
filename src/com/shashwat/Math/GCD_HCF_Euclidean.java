7c6b00dfc250d68a74994b6461c39450d3592cc6
Shashwat Gupta <shashwatg79802@gmail.com>
2022-08-18 19:33:06 +0530

    Adding HCF_Eulcidean & LCM

--- src/com/shashwat/Math/GCD_HCF_Euclidean.java
+++ src/com/shashwat/Math/GCD_HCF_Euclidean.java
@@ -1,6 +1,11 @@
 package com.shashwat.Math;
 
-public class GCD_HCF
+public class GCD_HCF_Euclidean
+
+    /*
+    Formula to calculate HCF using Euclidean's Algorithm is :
+    gcd (a, b) = gcd (remainder(b % a), a)
+     */
 {
     public static void main (String[] args) {
         System.out.println("GCD = " + gcd(224, 105));
